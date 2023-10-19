69579c3657c7d30aec3f56649f6e3af74b4ade01
Ekpei <iekpei@yahoo.com>
2023-10-19 17:07:00 +0100

    LOG UPDATED

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,51 +1,32 @@
 import sys
-import signal
+import re
+from collections import defaultdict
+
+log_format = r'(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)'
 
 total_file_size = 0
-status_code_counts = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
+status_code_counts = defaultdict(int)
+
 line_count = 0
-
-def signal_handler(sig, frame):
-    print_metrics()
-    sys.exit(0)
-
-def print_metrics():
-    print(f"Total file size: {total_file_size}")
-    for status_code in sorted(status_code_counts.keys()):
-        if status_code_counts[status_code] > 0:
-            print(f"{status_code}: {status_code_counts[status_code]}")
-
-signal.signal(signal.SIGINT, signal_handler)
 
 try:
     for line in sys.stdin:
-        line = line.strip()
-
-        import re
-        match = re.match(r'^(\d+\.\d+\.\d+\.\d+) - \[.*\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)$', line)
-        if not match:
-            continue
-
-        ip_address, status_code, file_size = match.groups()
-
-        total_file_size += int(file_size)
-        status_code = int(status_code)
-        if status_code in status_code_counts:
+        match = re.match(log_format, line)
+        if match:
+            line_count += 1
+            ip, date, status_code, file_size = match.groups()
+            total_file_size += int(file_size)
             status_code_counts[status_code] += 1
 
-        line_count += 1
-
-        if line_count % 10 == 0:
-            print_metrics()
-
+            if line_count % 10 == 0:
+                print("Total file size: File size:", total_file_size)
+                for code in sorted(status_code_counts):
+                    if code in ['200', '301', '400', '401', '403', '404', '405', '500']:
+                        print(f"{code}: {status_code_counts[code]}")
+        
 except KeyboardInterrupt:
-    print_metrics()
+    print("Keyboard interruption received. Printing current statistics:")
+    print("Total file size: File size:", total_file_size)
+    for code in sorted(status_code_counts):
+        if code in ['200', '301', '400', '401', '403', '404', '405', '500']:
+            print(f"{code}: {status_code_counts[code]}")
