299195ec7eb760e982ac2eb55701fbe305b7ee1c
Ekpei <iekpei@yahoo.com>
2023-11-29 08:37:29 +0100

    Island Perimeter updated

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,31 +2,17 @@
 def island_perimeter(grid):
     if not grid or not grid[0]:
         return 0
-
-    rows, cols = len(grid), len(grid[0])
+    
     perimeter = 0
+    rows, cols = len(grid), len(grid[0])
 
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4  # Start with the assumption of a full perimeter
-
-                # Check left neighbor
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2  # Subtract 2 for the shared edge
-
-                # Check above neighbor
+                perimeter += 4
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2  # Subtract 2 for the shared edge
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
 
     return perimeter
-
-grid = [
-    [0, 1, 0, 0],
-    [1, 1, 1, 0],
-    [0, 1, 0, 0],
-    [1, 1, 0, 0]
-]
-
-result = island_perimeter(grid)
-print(result)
